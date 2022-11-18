9da35791a1f6f6b465564c742c41a35e8645db80
ismahaneyy <liciousmayrah@gmail.com>
2022-11-18 17:49:59 +0300

    Done assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b){
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+}
+function subtract(a,b){
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+}
+function multiply(a,b){
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+}
+function divide(a,b){
+    a = Math.floor(Math.random() * 1000)
+    b = Math.floor(Math.random() * 1000)
+}
+
+function add(a, b) {
+   return (a + b)
+}
+function subtract(a,b){
+    return (a - b)
+}
+function multiply(a,b){
+    return (a * b)
+}
+function divide(a,b){
+    return (a/b)
+}
+function increment(n){
+    return (++n);
+}
+function decrement(n){
+    return (--n);
+}
+function makeInt(string){
+    return parseInt(string, 10);
+}
+function preserveDecimal(string){
+    return parseFloat(string);
+}
